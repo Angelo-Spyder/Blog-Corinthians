e59029d1968e5a8c0b3ab410a9cac628ae5c5cb1
Angelo Gabriel <Angello.Dev.42@gmail.com>
2022-06-04 19:40:53 -0300

    Carrossel agora interativo e funcional

---
+++ js/carrossel.js
@@ -0,0 +1,62 @@
+
+var bolinhas = document.querySelector(".bolinhas");
+var quantidade = document.querySelectorAll(".slide .imagem");
+var imagen = document.getElementById("atual");
+var proximo = document.querySelector("#proximo");
+var voltar = document.querySelector("#voltar");
+var atual = 0;
+var rolar = true;
+
+for(let i = 0; i < quantidade.length; i++){
+    var divBolinhas = document.createElement("div");
+    divBolinhas.id = i;
+
+    bolinhas.appendChild(divBolinhas);
+};
+
+bolinhas.addEventListener("click", function(event){
+    if(event.target.id != ""){
+        atual = event.target.id
+        rolar = false
+        slide()
+    }
+
+})
+
+/*INTERAÇÃO DO USUÁRIO NO CARROSEEL*/
+
+//Adicionando mudança de cor nsa bolinhas
+document.getElementById("0").classList.add("imgAtual")
+
+voltar.addEventListener("click", function(){
+    atual--
+    rolar = false
+    slide()
+})
+
+proximo.addEventListener("click", function(){
+    atual++
+    rolar = false
+    slide()
+})
+
+function slide(){
+
+    if(atual >= quantidade.length){
+        atual = 0
+    }else if(atual < 0){
+        atual = quantidade.length - 1
+    }
+    document.querySelector(".imgAtual").classList.remove("imgAtual");
+    imagen.style.marginLeft = -1024*atual + "px";
+    document.getElementById(atual).classList.add("imgAtual");
+}
+
+setInterval(()=>{
+    if(rolar){
+        atual++
+        slide()
+    }else{
+        rolar = true
+    }
+},4000)
